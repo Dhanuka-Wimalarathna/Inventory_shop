e1095f0815ad59a04c6b94bce580695ad24654fa
Dhanuka-Wimalarathna <143159213+Dhanuka-Wimalarathna@users.noreply.github.com>
2024-08-29 03:28:49 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_HOST', 'inventoryshop-server.mysql.database.azure.com' );          // Set database host
+  define( 'DB_USER', 'cmaafnxmrh' );             // Set database user
+  define( 'DB_PASS', '4lyKxq7$uC3mzUeW' );             // Set database password
   define( 'DB_NAME', 'inventory_system' );        // Set database name
 
 ?>
